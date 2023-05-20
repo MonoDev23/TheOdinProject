32f24b37d504907399df43b4cc92a340f2190ac9
MonoDev23 <tony.digi23@yahoo.com>
2023-05-20 18:42:25 +0200

    JS

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,37 @@
         // Your javascript goes here !
         // Console.log("") est la commande pour imrpimer quelque chose sur la console du développeur dans votre navigateur
         console.log("hello Mono");
+        
+       /* // TERMES "UNARY", "BINARY", "OPERAND"
+        let x = 1;
+        x = -x;
+        alert(x); // -1, unary negation was applied*/
+
+        /*let x = 1, y = 3;
+        alert(y - x); // 2, binary minus subtracts values*/
+
+        // ODIN PROJECT - AFFECTATION - PARTIE 1
+        //console.log(23 + 97);
+        // Solution à l'équation:(4 + 6 + 9) / 77
+        let x = (4 + 6 + 9) / 77; // 0.24675 résultat approximatif
+        //alert(x);
+        //console.log(x);
+
+        /*let a = 10;
+        let b = 7 * a;
+        console.log(9 * a);
+        console.log(b);*/
+
+       /* const max = 57;
+        const actual = max - 13;
+        const pourcentage = actual / max;
+        console.log(pourcentage);
+        //console.log(actual);*/
+
+
+
+
+
+
+
+        
