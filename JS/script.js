895bbae0338abfe9e6ee948faa046746d117c570
MonoDev23 <tony.digi23@yahoo.com>
2023-05-21 14:47:36 +0200

    JS

--- JS/script.js
+++ JS/script.js
@@ -28,6 +28,81 @@
         console.log(pourcentage);
         //console.log(actual);*/
 
+        // ODIN PROJECT - AFFECTATION - PARTIE 2
+
+        // Strings
+        /*const string = "The revolution will not be televised";
+        console.log(string);
+        const badString = string;
+        console.log(badString);*/ // string
+
+        //Singles quotes vs oduble quotes
+        /*const sgl = 'Single quotes';
+        const dbl = "Double quotes";
+        console.log(sgl); // Single quotes
+        console.log(dbl); // Double quotes
+
+        const sglDbl = 'Would you eat a "fish supper"';
+        const dblSgl = "I'm feeling blue";
+        console.log(sglDbl); // Would you eat a "fish supper"
+        console.log(dblSgl);*/ // I'm feeling blue
+
+        // Escaping characters in a string
+
+        /*const bigmouth = 'I\'ve got no right to take my place...';
+        console.log(bigmouth);*/
+
+        // Concatenating strings
+
+        /*const greet = `Hello`;
+        const name = `Tony`;
+        const greeting = `Hello, ${name}`;
+        console.log(greeting); // "Hello, Chris"*/
+
+        /*const one = "hello, ";
+        const two = "how are you";
+        const joined = `${one}${two}`;
+        console.log(joined); // "Hello, how are you?"*/
+
+        // Concatenation in context
+        
+        /*const button = document.querySelector("button");
+
+        function greet(){
+                const name = prompt("what your name?");
+                alert(`Hello ${name}, nice to see you`);
+        }
+        button.addEventListener("click", greet);*/
+
+        // Concatenation using "+"
+
+        /*const greeting = "Hello";
+        const name = "Tony";
+        console.log(greeting + ", " + name);*/ // "Hello, Tony"
+
+        // Number vs strings
+
+        /*const name = "Front";
+        const number = 230;
+        console.log(`${name}${number}`);*/ // "Front 230"
+
+        // Including expressions in strings
+
+        /*const song = "Fight the Youth";
+        const score = 9;
+        const highestScore = 10;
+        const output = `I like the song ${song}, I gave it a score of ${
+                (score / highestScore) * 100
+        }%.`;
+        console.log(output);*/ // I like the song Fight the Youth, I gave it a score of 90%.
+
+        // EXERCICES THE ODIN PROJECT VIA REPLIT.COM de 1 à 4
+
+
+
+
+
+
 
 
 
